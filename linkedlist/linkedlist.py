e9735679f18ea5b45b03efd0019bc468060fc437
Rajan Patel <rp581998@gmail.com>
2020-07-29 00:44:45 +0530

    Linked list introduction

---
+++ linkedlist/linkedlist.py
@@ -0,0 +1,55 @@
+class ListNode:
+    def __init__(self, value):
+        self.next = None
+        self.data = value
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insertAtEnd(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = node
+        return self.head
+    
+    def printList(self):
+        if self.head:
+            temp = self.head
+            while temp is not None:
+                print(temp.data, end = ' ')
+                temp = temp.next
+        print()
+    def push(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+        return self.head
+    def insertAt(self, position, node):
+        if self.head:
+            count = 0
+            temp = self.head
+            while count < position - 1:
+                temp = temp.next
+                count +=1
+            node.next = temp.next
+            temp.next = node
+        return self.head
+
+
+node = ListNode(1)
+list = LinkedList()
+list.insertAtEnd(node)
+list.insertAtEnd(ListNode(2))
+list.insertAtEnd(ListNode(3))
+list.insertAtEnd(ListNode(4))
+list.insertAtEnd(ListNode(5))
+list.printList()
+list.push(ListNode(10))
+list.printList()
+list.insertAt(4,ListNode(11))
+list.printList()
